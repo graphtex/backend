6d47430abf692a35c74110507e2aead0faee0793
Don Assamongkol <dassamongkol@arbok.cs.uchicago.edu>
2023-03-25 16:25:02 -0500

    set up route to accept image url. very basic Flask + cors set up

---
+++ hello.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
+
+# from PIL import Image
+
+app = Flask(__name__)
+
+# allow our Javascript app hosted on a different port (and thus origin) to access this Flask app
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/", methods=["GET", "POST"])
+@cross_origin()
+def index():
+    if request.method == "POST":
+        request_data = request.get_json()
+
+        # process the data here
+        img_url = request_data["url"]
+
+        # grab the image file from the url, convert to some python object i.e. PIL Image..   # img = Image().open("filename.png")
+        # run_inference :: Image -> (Boxes, Labels)
+        # translate_tikz :: (Boxes, Labels) -> Text
+
+        return {"code": "blah"}
